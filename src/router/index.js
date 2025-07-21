f0c8dbc9452eecd737d0a326f013da9096a869e1
Dylan Hanique <dylan.hanique@proton.me>
2025-07-21 15:37:45 +0200

    refactor: remove useless environment variables

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import { useUserStore } from '@/stores/useUserStore'
 import MoviesView from '@/views/MoviesView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory("localhost"),
   routes: [
     {
       path: '/',
--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_TMDB_API_BASE_URL,
+  baseURL: "https://api.themoviedb.org/3/",
   headers: {
     accept: 'application/json',
     'content-type': 'application/json',
